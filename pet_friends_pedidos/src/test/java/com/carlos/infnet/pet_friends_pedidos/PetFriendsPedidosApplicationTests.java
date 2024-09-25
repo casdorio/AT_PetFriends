9536f3efd5a8a2d4c5e2b689fa5572a2a32c53c4
casdorio <casdorio@gmail.com>
2024-09-25 17:36:23 -0300

    iniciando estruturas

---
+++ pet_friends_pedidos/src/test/java/com/carlos/infnet/pet_friends_pedidos/PetFriendsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.infnet.pet_friends_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFriendsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pet_friends_transporte/src/main/java/com/carlos/infnet/pet_friends_transporte/PetFriendsTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.infnet.pet_friends_transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFriendsTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFriendsTransporteApplication.class, args);
+	}
+
+}
